ee2a32a0752b180f271eb136a17c3bdc78f62863
n53f <n53337@users.noreply.github.com>
2023-01-29 15:44:39 +0000

    dataTypes init

---
+++ 0x02_DataTypes/dataTypes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	// integers
+	int num  = 12;
+	
+	// characters
+	char txt = 'H';
+
+	// floats
+	float fnum = 12.23;
+	
+	// Print Data Types
+	printf("int: %i\n char: %c\n float: %f\n", num, txt, fnum);
+
+	return 0;
+
+}
