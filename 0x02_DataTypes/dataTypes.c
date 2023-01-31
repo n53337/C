8b20271bf178b44c91cca17798b5f842894bf0a6
n53f <n53337@users.noreply.github.com>
2023-01-31 16:07:49 +0000

    C dataTypes

--- 0x02_DataTypes/dataTypes.c
+++ 0x02_DataTypes/dataTypes.c
@@ -13,6 +13,10 @@ int main(){
 	// Print Data Types
 	printf("int: %i\n char: %c\n float: %f\n", num, txt, fnum);
 
+	// I/O
+	int user_num;
+	scanf("%d", &user_num);
+	printf("\n%d\n", user_num);
 	return 0;
 
 }
