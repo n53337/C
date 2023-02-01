9aeed5734c843c2d2983f62c009401954deb9581
n53f <n53337@users.noreply.github.com>
2023-02-01 08:27:13 +0000

    adding user Input and printing function

--- 0x03_Mario/mario.c
+++ 0x03_Mario/mario.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 
-int main(){
-  int bricks = 12;
-
-  // print Bricks
-  for(int i = 0; i<bricks / 2; i++){
-    for(int j = 0; j<bricks; j++){
+// Display Blocks function
+void render_blocks(int blocks){
+   // print Bricks
+  for(int i = 0; i < blocks / 2; i++){
+    for(int j = 0; j < blocks; j++){
       printf("#");
     }
     printf("\n");
   }
-  printf("\n");
+
+}
+
+// Main Function
+int main(){
+  int blocks;
+
+  // user Input
+  printf("===== WELCOME TO MARIO =====\n");
+  printf("ENTER NUMBER OF BLOCKS: ");
+  scanf("%i",&blocks);
+  
+  // verify user input
+  if(blocks > 0){
+    render_blocks(blocks);
+  }
+  else main();
+
   return 0;
 }
+
+
