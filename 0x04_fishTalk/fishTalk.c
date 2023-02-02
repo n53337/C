82186dcc73d6bd1efcb8a7375f11498a498b9ab6
n53f <n53337@users.noreply.github.com>
2023-02-02 10:25:20 +0000

    display name with fish draw

--- 0x04_fishTalk/fishTalk.c
+++ 0x04_fishTalk/fishTalk.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
-void print_fish(char name[]);
+void print_fish(char name[], int args);
 
 int main(int argc, char **argv)
 {
    // Deal With argv char
    //printf("%s\n", argv[1]);
-   print_fish(argv[1]);
+   print_fish(argv[1], argc);
 }
 
-void print_fish(char name[]){
-   printf("%s\n", name);
+void print_fish(char name[], int args){
+   printf("\n");
+   if(args - 1) printf("%s /\n", name); 
+   else printf("fish talk /\n");
+   printf("    _/./\n");
+   printf(" ,-'    `-:.,-'/\n");
+   printf(" > O )<)    _  (\n");
+   printf(" `-._  _.:' `-./\n");
+   printf("     `` ;\n");
+   printf("\n");
 }
