a385017c68fa9c7450d221fc174d12ce7b0484d7
n53f <n53337@users.noreply.github.com>
2023-02-02 10:26:36 +0000

    delete some comments

--- 0x04_fishTalk/fishTalk.c
+++ 0x04_fishTalk/fishTalk.c
@@ -4,12 +4,11 @@ void print_fish(char name[], int args);
 
 int main(int argc, char **argv)
 {
-   // Deal With argv char
-   //printf("%s\n", argv[1]);
    print_fish(argv[1], argc);
 }
 
 void print_fish(char name[], int args){
+   // Fish art
    printf("\n");
    if(args - 1) printf("%s /\n", name); 
    else printf("fish talk /\n");
